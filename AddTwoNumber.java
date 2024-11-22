836bb70345c99d8facf48435514a3f68fc5664a9
shyam sharan <kumarssaran413@gmail.com>
2024-11-22 20:36:08 +0530

    add two number

---
+++ AddTwoNumber.java
@@ -0,0 +1,9 @@
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Shyam!");
+        System.out.println(5 + 4 );
+
+         
+    }
+}
